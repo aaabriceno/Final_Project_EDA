d3752149da4d96d84d065a7f3df046e386fe7e8e
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2025-06-11 22:54:14 -0500

    Añadiendo Funcion Split y subfunciones

--- Estructura/GeoCluster.cpp
+++ Estructura/GeoCluster.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 //const int MAX_NIVEL = 5; //Maximo nivel de nodos
 const int MAX_PUNTOS_POR_NODO = 50; //Maximo de puntos por nodo
-
+const int MIN_PUNTOS_POR_NODO = 20;
 //Estructura Punto
 struct Punto{
     int id;
@@ -76,6 +76,7 @@ public:
 
 private:
     Nodo* raiz;
+
     //funcion chooseSubTree y sus funciones internas
     Nodo* chooseSubTree(const Punto& punto);
     double calcularOverlapCosto(const MBR& mbr, const Punto& punto);
@@ -85,7 +86,10 @@ private:
     //funcion Split y sus funciones internas
     void Split(Nodo* nodo, Nodo*& nuevo_nodo);
     int chooseSplitAxis(const vector<Punto>& puntos);
-    int chooseSplitIndex(const vector<Punto>& puntos, int eje);
+    double calcularMargen(const vector<Punto>& puntosLat);
+    MBR calcularMBR(const vector<Punto>& puntos);
+    double calcularOverlap(const MBR& mbr1, const MBR& mbr2);
+    int chooseSplitIndex(vector<Punto>& puntos, int eje);
 
     //
     Nodo* findBestLeafNode(const Punto& punto);
@@ -97,20 +101,101 @@ private:
     void searchRec(const MBR& rango, Nodo* nodo, vector<Punto>& puntos_similares);
 };
 
-int GeoCluster::chooseSplitAxis(const vector<Punto>& puntos){
-    double margeLat = 0.0, margenLong = 0.0;
 
+MBR GeoCluster::calcularMBR(const vector<Punto>& puntos) {
+    double minLat = puntos[0].latitud, maxLat = puntos[0].latitud;
+    double minLon = puntos[0].longitud, maxLon = puntos[0].longitud;
+
+    // Calcular los valores mínimo y máximo de latitud y longitud
+    for (const auto& punto : puntos) {
+        minLat = std::min(minLat, punto.latitud);
+        maxLat = std::max(maxLat, punto.latitud);
+        minLon = std::min(minLon, punto.longitud);
+        maxLon = std::max(maxLon, punto.longitud);
+    }
+
+    // Crear el MBR que cubre todos los puntos
+    return MBR(minLat, minLon, maxLat, maxLon);
+}
+
+double GeoCluster::calcularOverlap(const MBR& mbr1, const MBR& mbr2) {
+    double lat_overlap = std::max(0.0, std::min(mbr1.m_maxp[0], mbr2.m_maxp[0]) - std::max(mbr1.m_minp[0], mbr2.m_minp[0]));
+    double lon_overlap = std::max(0.0, std::min(mbr1.m_maxp[1], mbr2.m_maxp[1]) - std::max(mbr1.m_minp[1], mbr2.m_minp[1]));
+
+    return lat_overlap * lon_overlap;  // El área de la intersección
+}
+
+
+int GeoCluster::chooseSplitAxis(const vector<Punto>& puntos){
+    //double margeLat = 0.0, margenLong = 0.0;  
     //Paso 1: Ordenar por latitud(eje 0) y longitud(eje 1)
     vector<Punto> puntosLatitud = puntos;
-    vector<Punto> puntoslongitud = puntos;
+    vector<Punto> puntosLongitud = puntos;
 
-        //ordenar los puntos 
+    //ordenar los puntos 
+    sort(puntosLatitud.begin(), puntosLatitud.end(), [](const Punto& p1, const Punto& p2) {
+        return p1.latitud < p2.latitud;
+    });
 
+    sort(puntosLongitud.begin(), puntosLatitud.end(),[](const Punto& p1, const Punto& p2) {
+        return p1.longitud < p2.longitud;
+    });
+
+    //Paso 2: Calcular el numero de distribuciones (M - 2m + 2) para cada eje
+    int M = MAX_PUNTOS_POR_NODO;
+    int m = M/2;
+    
+    int numero_distribuciones_lat = M - 2*m + 2;
+    int numero_distribuciones_long = M - 2*m + 2;
+
+    //Paso 3:Calcular el margen para cada eje
+    double margenLat = calcularMargen(puntosLatitud);
+    double margenLong = calcularMargen(puntosLongitud);
+
+    return (margenLat < margenLong) ? 0:1;
 }
 
-int GeoCluster::chooseSplitIndex(const vector<Punto>& puntos, int eje){
+int GeoCluster::chooseSplitIndex(vector<Punto>& puntos, int eje) {
+    int M = puntos.size();  // Número total de puntos
+    int m = M / 2;  // Aproximadamente la mitad de los puntos
 
+    // Ordenar los puntos según el eje seleccionado (latitud o longitud)
+    if (eje == 0) {
+        sort(puntos.begin(), puntos.end(), [](const Punto& p1, const Punto& p2) {
+            return p1.latitud < p2.latitud;  // Ordenar por latitud
+        });
+    } else {
+        sort(puntos.begin(), puntos.end(), [](const Punto& p1, const Punto& p2) {
+            return p1.longitud < p2.longitud;  // Ordenar por longitud
+        });
+    }
+
+    // Paso 1: Calcular las distribuciones posibles (M - 2m + 2)
+    // Para cada índice de división, calculamos el solapamiento de los MBRs
+    double minOverlap = numeric_limits<double>::infinity();
+    int bestSplitIndex = 0;
+
+    for (int i = 1; i < M; ++i) {  // Para cada posible división
+        vector<Punto> grupo1(puntos.begin(), puntos.begin() + i);  // Grupo 1 con i puntos
+        vector<Punto> grupo2(puntos.begin() + i, puntos.end());   // Grupo 2 con el resto de los puntos
+
+        // Paso 2: Calcular los MBRs para cada grupo
+        MBR mbrGrupo1 = calcularMBR(grupo1);
+        MBR mbrGrupo2 = calcularMBR(grupo2);
+
+        // Paso 3: Calcular el solapamiento entre los MBRs
+        double overlap = calcularOverlap(mbrGrupo1, mbrGrupo2);
+
+        // Paso 4: Seleccionar el índice con el menor solapamiento
+        if (overlap < minOverlap) {
+            minOverlap = overlap;
+            bestSplitIndex = i;
+        }
+    }
+
+    return bestSplitIndex;  // Devolver el índice con el menor solapamiento
 }
+
 
 void Split(Nodo* nodo, Nodo* nuevo_nodo){
 
