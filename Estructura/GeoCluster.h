45df21b55d23a5e16433f18ff6968de478136868
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2025-06-13 14:05:19 -0500

    Agregando GeoCluster.h y main.cpp

---
+++ Estructura/GeoCluster.h
@@ -0,0 +1,42 @@
+#ifndef GEOCLUSTER_H
+#define GEOCLUSTER_H
+
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <array>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+//const int MAX_NIVEL = 5; //Maximo nivel de nodos
+const int MAX_PUNTOS_POR_NODO = 30; //Maximo de puntos por nodo
+const int MIN_PUNTOS_POR_NODO = 10;
+
+//Estructura Punto
+struct Punto{
+    int id;
+    double latitud, longitud;
+    vector <double> atributos; //12 attributos a 6-8 aproximadamente
+    int id_cluster_geografico, id_subcluster_atributivo;
+};
+
+//Estructura MBR
+struct MBR{
+    double m_minp[2]; // [latitud minima, longitud minima]
+    double m_maxp[2]; // [latitud maxima, longitud maxima]
+
+    MBR(){}
+    MBR(double min_latitud, double min_longitud, double max_latitud, double max_longitud) {
+        m_minp[0] = min_latitud; // latitud minima
+        m_minp[1] = min_longitud; // longitud minima
+        m_maxp[0] = max_latitud; // latitud maxima
+        m_maxp[1] = max_longitud; // longitud maxima
+    }
+};
+
+
+#endif
