e69927bcef612d9815d1ba55e4c7397ced85b72c
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2025-06-17 15:53:00 -0500

    Eliminando Struct Hoja y Corrigiendo codigo

--- Estructura/GeoCluster.h
+++ Estructura/GeoCluster.h
@@ -22,6 +22,7 @@ struct Punto{
     vector <double> atributos; //12 attributos a 6-8 aproximadamente
     int id_cluster_geografico, id_subcluster_atributivo;
 };
+
 //Estructura MBR
 struct MBR{
     double m_minp[2]; // [latitud minima, longitud minima]
@@ -33,6 +34,25 @@ struct MBR{
         m_minp[1] = min_longitud; // longitud minima
         m_maxp[0] = max_latitud; // latitud maxima
         m_maxp[1] = max_longitud; // longitud maxima
+    }
+
+    void reset(){
+         m_minp[0] = m_minp[1] = std::numeric_limits<double>::max();
+        m_maxp[0] = m_maxp[1] = std::numeric_limits<double>::lowest();
+    }
+
+    // Método que uso para  estirar el MBR para incluir otro MBR
+    void stretch(const MBR& other) {
+        m_minp[0] = std::min(m_minp[0], other.m_minp[0]);
+        m_minp[1] = std::min(m_minp[1], other.m_minp[1]);
+        m_maxp[0] = std::max(m_maxp[0], other.m_maxp[0]);
+        m_maxp[1] = std::max(m_maxp[1], other.m_maxp[1]);
+    }
+
+    // Método que uso para verificar si dos MBRs se intersectan
+    bool is_intersected(const MBR& other) const {
+        return !(m_maxp[0] < other.m_minp[0] || m_maxp[1] < other.m_minp[1] || 
+                 m_minp[0] > other.m_maxp[0] || m_minp[1] > other.m_maxp[1]);
     }
 };
 
@@ -51,61 +71,53 @@ struct MicroCluster{
 //Estructura Nodo GeoCluster
 struct Nodo{
     bool esHoja;
+    int m_nivel;
     MBR mbr;
     vector<Nodo*> hijos;  
-    Nodo(bool esHoja = false):esHoja(esHoja){ }
-};
-
-struct Hoja{
-    vector<Punto> puntos;
-    vector<MicroCluster> microCluster_hoja;
-    MBR mbr_hoja;
+    vector<Punto> puntos; //Solo para las hojas
+    vector<MicroCluster> microclusters_en_Hoja;
+    Nodo(bool esHoja = false) : esHoja(esHoja), m_nivel(0) {}
 };
 
 //Clase GeoCluster - Tree
 class GeoCluster {  
 public:
-    GeoCluster(); //Constructor
-    ~GeoCluster(); //Destructor
-    Nodo* getRaiz(){ return raiz;} //retorna el nodo raiz (private)
+    GeoCluster();
+    ~GeoCluster();
+    Nodo* getRaiz(){ return raiz;}
     
-    //Funcion que inserta los puntos
     void insertarPunto(const Punto& punto);
-    //Funcion consulta 1
-    vector<Punto> n_puntos_similiares_a_punto(const Punto& punto_de_busqueda, MBR& rango,int numero_de_puntos_similares);
-    //Funcion consulta 2
+    vector<Punto> n_puntos_similiares_a_punto(const Punto& punto_de_busqueda, MBR& rango, int numero_de_puntos_similares);
     vector<vector<Punto>> grupos_similares_de_puntos(MBR& rango);
     
     vector<Punto> buscarPuntosDentroInterseccion(const MBR& rango, Nodo* nodo);
     MBR calcularMBR(const vector<Punto>& puntos);
+
 private:
     Nodo* raiz;
 
-    //funcion chooseSubTree y sus funciones internas
     double calcularOverlapCosto(const MBR& mbr, const Punto& punto);
     double calcularAreaCosto(const MBR& mbr, const Punto& punto);
     double calcularMBRArea(const MBR& mbr);
-    Nodo* chooseSubTree(const Punto& punto);
+    Nodo* chooseSubTree(Nodo* nodo, const Punto& punto);
     
-    
-    //funcion Split y sus funciones internas
     int chooseSplitAxis(const vector<Punto>& puntos);
     int chooseSplitIndex(vector<Punto>& puntos, int eje);
     double calcularOverlap(const MBR& mbr1, const MBR& mbr2);
     double calcularMargen(const MBR& mbr);
     void Split(Nodo* nodo, Nodo*& nuevo_nodo);
     
-
-    //Funciones Extras
     Nodo* findBestLeafNode(const Punto& punto);
     void insertIntoLeafNode(Nodo* nodo_hoja, const Punto& punto);
-    void insertIntoParent(Nodo* nodo_hoja, Nodo* nuevo_nodo);
+    void insertIntoParent(Nodo* padre, Nodo* nuevo_nodo);
     void updateMBR(Nodo* nodo);
     bool interseccionMBR(const MBR& mbr1, const MBR& mbr2);
     void searchRec(const MBR& rango, Nodo* nodo, vector<Punto>& puntos_similares);
     MBR computeMBR(double ax1, double ay1, double ax2, double ay2,
                    double bx1, double by1, double bx2, double by2);
     bool estaDentroDelMBR(const Punto& punto, const MBR& mbr);
+    double computeArea(double ax1, double ay1, double ax2, double ay2,
+                      double bx1, double by1, double bx2, double by2);
 };
 
 #endif
